7ae703467827350cd9fddd617605403088d73323
ghoshbinayak <ghoshbinayak@gmail.com>
2014-11-02 22:55:13 +0530

    bugfix

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from accounts.models import robodarshanMember
 
 
 class story(models.Model):
-    uuid = models.CharField(max_length=32, unique=True)
+    uuid = models.CharField(max_length=32, primary_key=True)
     title = models.CharField(max_length=256)
     subtitle = models.TextField()
     body = models.TextField()
