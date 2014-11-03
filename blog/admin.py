a99d1846886a554811b713bda36dc803b4f64b79
ghoshbinayak <ghoshbinayak@gmail.com>
2014-11-03 17:17:46 +0530

    started work on admin interface

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,40 @@ from django.contrib import admin
 from blog.models import story
 
 
+def make_published(modeladmin, request, queryset):
+    queryset.update(published=True)
+make_published.short_description = "Published selected stories"
+
+
+def unpublish(modeladmin, request, queryset):
+    queryset.update(published=False)
+unpublish.short_description = "Unpublish selected stories"
+
+
 class storyAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('story_text', 'author_name', 'timestamp', 'published')
+    date_hierarchy = 'timestamp'
+    readonly_fields = ('author_name',)
+    fieldsets = (
+        ('Edit Story', {
+            'fields': ('title', 'author_name', 'body')
+        }),
+        ('Publish Story', {
+            'classes': ('extrapretty'),
+            'fields': ('published',)
+        }),
+    )
+    list_filter = ['published', 'timestamp']
+    search_fields = ['title']
+    actions = [make_published, unpublish]
+
+    def story_text(self, obj):
+        return obj.__unicode__()
+    story_text.short_description = 'Story Title'
+
+    def author_name(self, obj):
+        return obj.author.get_full_name()
+    author_name.short_description = 'Author'
 
 
 admin.site.register(story, storyAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,9 +5,10 @@ from accounts.models import robodarshanMember
 class story(models.Model):
     uuid = models.CharField(max_length=32, primary_key=True)
     title = models.CharField(max_length=256)
-    subtitle = models.TextField()
     body = models.TextField()
     timestamp = models.DateTimeField()
-    permalink = models.CharField(max_length=256)
     author = models.ForeignKey(robodarshanMember)
     published = models.BooleanField(default=False)
+
+    def __unicode__(self):
+        return self.title
