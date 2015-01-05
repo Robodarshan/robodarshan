691977bd1e065da41d3c4cc01d48094d26d1e3f1
ghoshbinayak <ghoshbinayak@gmail.com>
2015-01-06 03:51:05 +0530

    modified requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='robodarshan',
-      version='0.5',
+      version='0.51',
       description='Website for Robodarshan, the robotics club of IIEST Shibpur',
       author='Binayak Ghosh, ',
       author_email='ghoshbinayak@gmail.com',
