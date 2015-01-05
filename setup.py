9ac838f2027f30ce0e89ab4703e4f4d3e234f83a
ghoshbinayak <ghoshbinayak@gmail.com>
2015-01-06 02:34:51 +0530

    added app.py, setup.py for openshift

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='robodarshan',
+      version='0.5',
+      description='Website for Robodarshan, the robotics club of IIEST Shibpur',
+      author='Binayak Ghosh, ',
+      author_email='ghoshbinayak@gmail.com',
+      url='',
+#      install_requires=['Django>=1.3'],
+     )
